450cf1760f12a60a25ad24d75b660542e1fccecb
lya aleksandrov <01.11.2001-01.11.2001@mail.ru>
2022-07-10 10:12:13 +0300

    create dir for second homework

---
+++ Seminar2/tree/Person.java
@@ -0,0 +1,12 @@
+
+class Person {
+    public String fullName;
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public Person(String fullName) {
+        this.fullName = fullName;
+    }
+}
