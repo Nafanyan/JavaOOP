ac6bc44387450a14eb852a98f06dd561782eb830
lya aleksandrov <01.11.2001-01.11.2001@mail.ru>
2022-07-18 17:43:48 +0300

    создал файл для пользовательского взаимодействия, консоль

--- Seminar4/userFile/Program.java
+++ Seminar4/userFile/Program.java
@@ -1,5 +1,12 @@
+package userFile;
+
+import presenter.*;
+
 public class Program {
     public static void main(String[] args) {
+        Presenter hi = new Presenter();
+        hi.button();
+        
         
     }
 }
