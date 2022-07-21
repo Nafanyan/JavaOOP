170dd14c2b66113addc41a935812f2e2758a0b23
lya aleksandrov <01.11.2001-01.11.2001@mail.ru>
2022-07-21 18:38:07 +0300

    наследование от юзера, конкретный клиент

---
+++ Seminar5/Client.java
@@ -0,0 +1,29 @@
+
+public class Client extends User {
+  private ICQ chatroom;
+
+  public Client(String name) {
+    this.name = name;
+  }
+
+  void printMessage(String msg) {
+    System.out.printf("Chat, %s: %s\n", name, msg);
+  }
+
+  void sendMsg(String text) {
+    chatroom.sendMessage(text, this);
+  }
+
+  public void setChatroom(ICQ chatroom) {
+    this.chatroom = chatroom;
+  }
+
+  public void delClient(Client user) {
+    chatroom.delClient(this, user);
+  }
+
+  public void giveUserRights(UserRights whoUser, Client user) {
+    if (chatroom.getUsers().contains(this))
+      this.setWhoUser(whoUser, user);
+  }
+}
