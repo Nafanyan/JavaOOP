c9194760324b9cd37e9d538c8fb2ddac69ec1851
lya aleksandrov <01.11.2001-01.11.2001@mail.ru>
2022-07-21 18:37:22 +0300

    абстрактный класс Юзера с ролями

---
+++ Seminar5/User.java
@@ -0,0 +1,21 @@
+abstract public class User {
+    protected UserRights whoUser;
+    protected String name;
+    protected int id;
+
+    abstract void printMessage(String msg);
+    abstract void sendMsg(String text);
+
+    public void setWhoUser(UserRights whoUser, User user) {
+        if (this.whoUser == UserRights.ADMIN) {
+            user.whoUser = whoUser;
+        }
+    }
+
+}
+
+enum UserRights {
+    ADMIN,
+    MODERATOR,
+    SIMPLE
+}
