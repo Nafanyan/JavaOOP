694fc6b6214a705ffa91fc09f528c9b05301b665
Nafanya <90004716+Nafanyan@users.noreply.github.com>
2022-07-28 17:58:53 +0300

    Update ICQStorage.java

---
+++ Seminar5/ICQStorage.java
@@ -0,0 +1,32 @@
+package Seminar5;
+
+import java.util.LinkedList;
+import java.util.Date;
+
+public class ICQStorage implements Storage{
+    private LinkedList<String> storMsg = new LinkedList<>();
+    private ICQ chatroom;
+    Date d = new Date();
+
+    public ICQStorage(ICQ chatroom){
+        this.chatroom = chatroom;
+    }
+
+    @Override
+    public void addMsg(String msg, String user) {
+        storMsg.add("id: " + storMsg.size() + " " + user + ": " + msg + " - " + d);
+    }
+
+    @Override
+    public void delMsg(int id) {
+        storMsg.remove(id);
+    }
+
+    @Override
+    public void printMsg() {
+        for (String string : storMsg) {
+            System.out.println(string);
+        }  
+    }
+    
+}
